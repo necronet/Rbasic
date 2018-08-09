45d9d3f6c19e25c607e448707b9fc0087bd5ae99
Jose Ayerdis <necronet@Joses-MacBook-Pro.local>
2018-08-08 23:46:13 -0700

    Set everything in a kmeans functoin [refactoring]

--- clustering/kmeans.R
+++ clustering/kmeans.R
@@ -3,12 +3,12 @@ require(MASS)
 library(ggplot2)
 
 euclideanDist <- function (x,y) {
-  rowSums((x-y)^2)
+  sqrt(rowSums((x-y)^2))
 }
 
 show_cluster <- function(points,c_coords,classified){
   with(df, plot(points, col=classified,pch=1))
-  points(c_coords,col="red", pch=18)
+  points(c_coords,col="blue", pch=18)
 }
 
 # Using standard R clustering library
@@ -20,61 +20,71 @@ with(df, plot(df, col=k2$cluster,pch=1))
 points(k2$centers,col="red", pch=18)
 
 
+
 dev.off()
-set.seed(1)
-p <- 2
-x = mvrnorm(100, c(-2,4), Sigma=diag(p))
-y = mvrnorm(100, c(4,2), Sigma=diag(p))
+set.seed(100)
+
+sigma <- matrix(c(1,0.1,-0.4,1),2,2)
+#sigma2 <- matrix(c(0.1,3,-4,2),2,2)
+x = mvrnorm(100, c(-8,0.1), Sigma=sigma)
+y = mvrnorm(100, c(0,1), Sigma=sigma)
 df = as.data.frame( cbind(c(x),c(y)) )
+plot(df, pch=1)
 
 # Step 1. Specify K cluster
 k = 2
-
-# Step 2. Randomly select k objects from the dataset as initial clusters
-centroids = sample(nrow(df),k)
-#df[centroids,]
-# Step 3. Assign each observation to the closest centroid
-cluster = rep(0,nrow(df))
-cluster[cluster==2] <- 3
-
-centroids_points <- rep(unname(unlist(t(df[centroids,]))), nrow(df) )
-dim(centroids_points) <- c(length(unlist(df[centroids,])),nrow(df))
-centroids_points <- as.data.frame(t(centroids_points))
-
-from <- seq(1,k*p,p)
-distances <- matrix(rep(0,k*nrow(df)),ncol=2)
-
-for(i in 1:k) {
-  to <- i*p
-  distances[,i] <- euclideanDist(df,centroids_points[from[i]:to]) 
-}
-for(i in 1:k) {
-  cluster[which(distances[,i]==apply(distances,1,min))]<-i
-}
-distances <- as.data.frame(distances)
-
-show_cluster(df,df[centroids,],cluster)
-
-# Step 4. For each cluster k, update the centroid by calculating the new mean value
 iter <- 10
-prev_centroids <- df[centroids,]
-for( i in 1:iter){
+kmeans(k,df,iter)
+
+kmeans <- function(k,df,iter) {
+  # Step 2. Randomly select k objects from the dataset as initial clusters
+  centroids = sample(nrow(df),k)
+  p <- ncol(df)
+  #df[centroids,]
+  # Step 3. Assign each observation to the closest centroid
+  cluster = rep(0,nrow(df))
+  cluster[cluster==2] <- 3
   
-  centroid_coords <- sapply(1:k, function(i,df,cluster) colMeans(df[cluster==i,]), df, cluster)
-  centroids_points <- matrix( rep(as.vector(t(centroids_coords)),each=nrow(df)),nrow=nrow(df))
+  centroids_points <- rep(unname(unlist(t(df[centroids,]))), nrow(df) )
+  dim(centroids_points) <- c(length(unlist(df[centroids,])),nrow(df))
+  centroids_points <- as.data.frame(t(centroids_points))
   
-  if(all(prev_centroids == centroid_coords)){
-    print(sprintf("Converge point, mean is no longer changing after %d iteration", i))
-    break
-  }
+  from <- seq(1,k*p,p)
+  distances <- matrix(rep(0,k*nrow(df)),ncol=2)
+  
   for(i in 1:k) {
     to <- i*p
-    distances[,i] <- euclideanDist(df,centroids_points[,from[i]:to]) 
+    distances[,i] <- euclideanDist(df,centroids_points[from[i]:to]) 
   }
   for(i in 1:k) {
     cluster[which(distances[,i]==apply(distances,1,min))]<-i
   }
-  prev_centroids <- centroid_coords
+  distances <- as.data.frame(distances)
   
+  show_cluster(df,df[centroids,],cluster)
+  
+  # Step 4. For each cluster k, update the centroid by calculating the new mean value
+  
+  prev_centroids <- df[centroids,]
+  for( i in 1:iter){
+    
+    centroid_coords <- sapply(1:k, function(i,df,cluster) colMeans(df[cluster==i,]), df, cluster)
+    centroids_points <- matrix( rep(as.vector(t(centroid_coords)),each=nrow(df)),nrow=nrow(df))
+    
+    if(all(prev_centroids == centroid_coords)){
+      print(sprintf("Converge point, mean is no longer changing after %d iteration", i))
+      break
+    }
+    for(i in 1:k) {
+      to <- i*p
+      distances[,i] <- euclideanDist(df,centroids_points[,from[i]:to]) 
+    }
+    for(i in 1:k) {
+      cluster[which(distances[,i]==apply(distances,1,min))]<-i
+    }
+    prev_centroids <- centroid_coords
+    
+  }
+  show_cluster(df,prev_centroids,cluster)
+
 }
-show_cluster(df,centroids_coords,cluster)
